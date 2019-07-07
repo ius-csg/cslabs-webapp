16847f2adbfc33124f4c6922ec3e3c570ef3df10
Jason Gallavin <jasongallavin@gmail.com>
2019-07-07 03:55:16 -0400

    Add inputs for the screen ticket to test the console. Add centering to the console.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,33 +1,56 @@
 import * as React from 'react';
 import {log} from '../../util';
-import {Component} from 'react';
+import {ChangeEvent, Component} from 'react';
+import * as styles from './Home.module.scss';
+
+let wmks: any = null;
 
 class Home extends Component {
 
-  componentDidMount(): void {
-    initVM();
-  }
+  state = { ticket: '' };
+
+  onChange = (event: ChangeEvent<HTMLInputElement>) =>
+    this.setState({ ticket: event.target.value});
+
+  connectVM = () => {
+    if (this.state.ticket.length === 0 ) {
+      alert('Ticket must be entered');
+      return;
+    }
+    initVM(this.state.ticket);
+  };
+
+  sendCtrlAltDelete = () => {
+    if (wmks !== null) {
+      wmks.sendCAD();
+    }
+  };
 
   render() {
     return  (
-      <div id='wmksContainer'>
-        Hello
+      <div>
+        <input type='text' value={this.state.ticket} onChange={this.onChange} />
+        <button onClick={this.connectVM}>Connect</button>
+        <button onClick={this.sendCtrlAltDelete}>Send Ctrl + Alt + Delete</button>
+        <div className={styles['wmks-console-window-container']}>
+          <div id='wmksConsoleWindow'  className={styles['wmks-console-window']}/>
+        </div>
       </div>
     );
   }
 }
 
-function initVM() {
+function initVM(ticket: string) {
   const WMKS = window['WMKS'];
   log(WMKS);
-  const	wmks = window['WMKS'].createWMKS('wmksContainer', {})
+  wmks = WMKS.createWMKS('wmksConsoleWindow', {})
     .register(WMKS.CONST.Events.CONNECTION_STATE_CHANGE,
       (event: any, data: any) => {
         if (data.state === WMKS.CONST.ConnectionState.CONNECTED) {
           log('connection	state	change	:	connected');
         }
       });
-  // wmks.connect('ws://127.0.0.1:8080');
+  wmks.connect('wss://esxi.home.local/ticket/' + ticket);
 }
 
 export default Home;
