0e213d4320b3990a5aebc02f56a858303cecde08
Barel Musenga <barel.musenga87@gmail.com>
2019-09-05 19:50:37 -0400

    [added a public module for labs]

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -4,12 +4,14 @@ import Home from '../pages/Home/Home';
 import NotFound from '../pages/NotFound/NotFound';
 import History from './history';
 import Login from '../pages/Login/Login';
+import {PublicModule} from '../pages/public-module/Public-module';
 
 const Routes = () => (
   <Router history={History}>
     <Switch>
       <Route exact={true} path='/' component={Home}/>
       <Route exact={true} path='/Login' component={Login}/>
+      <Route exact={true} path='/Public-module' component={PublicModule}/>
       <Route component={NotFound} />
     </Switch>
   </Router>
