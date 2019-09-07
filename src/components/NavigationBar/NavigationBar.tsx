5c80bc8ebdc24630598d954400cd85987e025a9c
Barel Musenga <barel.musenga87@gmail.com>
2019-09-07 17:16:34 -0400

    Changed made following the Change Request
     Created a separe component called moduleCard which is later called in the explore page.
        Rename public module page into Eplore page and everywhere it was referenced in the program.
        Add a Layout component and the navbar with I am still working on.
        Fixed some issues per Jason change request.

---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Nav, Navbar, Container} from 'react-bootstrap';
+import styles from './NavigationBar.module.scss';
+
+export const NavigationBar = () => (
+  <Navbar expand='lg' className={styles.navbar}>
+    <Container>
+      <Navbar.Brand href='/' className={styles.navbarBrand}>CSG</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+      <Navbar.Collapse id={'basic-navbar-nav'} className={styles.navbarNav}>
+        <nav className={'mr-auto'}>
+          <Nav.Item><Nav.Link href={'/'}>Home > Explore</Nav.Link></Nav.Item>
+        </nav>
+        <Nav className={'ml-auto'}>
+          <Nav.Item><Nav.Link href={'/explore'}>Explore</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href={'/explore'}>Modules</Nav.Link></Nav.Item>
+          <Nav.Item><Nav.Link href={'/login'}>Account</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+)
---
+++ src/pages/Explore/Explore.tsx
@@ -0,0 +1,9 @@
+import {Container} from 'react-bootstrap';
+import React from 'react';
+import {ModuleCard} from '../../components/ModuleCard/ModuleCard';
+
+export const Explore = () => (
+    <Container>
+      <ModuleCard/>
+    </Container>
+)
