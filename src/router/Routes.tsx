4a479c640873c240623c4eddedc977e43cc0c9e2
Chase Benston <cbenston@ius.edu>
2019-09-07 12:43:11 -0400

    Added Profile Page
    Added profile page, profile css, and edited the links in the router to be lowercase.

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -4,12 +4,14 @@ import Home from '../pages/Home/Home';
 import NotFound from '../pages/NotFound/NotFound';
 import History from './history';
 import Login from '../pages/Login/Login';
+import Profile from '../pages/Profile/Profile';
 
 const Routes = () => (
   <Router history={History}>
     <Switch>
       <Route exact={true} path='/' component={Home}/>
-      <Route exact={true} path='/Login' component={Login}/>
+      <Route exact={true} path='/login' component={Login}/>
+      <Route exact={true} path='/profile' component={Profile}/>
       <Route component={NotFound} />
     </Switch>
   </Router>
