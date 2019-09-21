ae146f114eb990b5aaa6d5399f7b9a2a0ac43314
Chase Benston <cbenston@umail.iu.edu>
2019-09-21 15:41:42 -0400

    Reworked Reset Password
    Reworked reset password still need to add some css and the password security level.
    Added current password and confirm new password requirements

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,27 +1,65 @@
 import * as React from 'react'
 import {Component, FormEvent} from 'react';
-import {Button, Container, FormControlProps, Row, Col} from 'react-bootstrap';
+import {Button, Container, FormControlProps, Form, Col} from 'react-bootstrap';
 
 export default class ResetPassword extends Component {
 
-  onPasswordChange = (event: FormEvent<FormControlProps>) => {
-    this.setState({schoolEmail: event.currentTarget.value});
+  state = {
+    currentPass: '',
+    password: '',
+    confirmPass: ''
   };
-
+  onCurrentPasswordChange = (event: FormEvent<FormControlProps>) => {
+    this.setState({currentPass: event.currentTarget.value});
+  }
+  onPasswordChange = (event: FormEvent<FormControlProps>) => {
+    this.setState({password: event.currentTarget.value});
+  };
+  onConfirmPassChange = (event: FormEvent<FormControlProps>) => {
+    this.setState({confirmPass: event.currentTarget.value});
+  }
+  isPassInvalid = () => {
+    return this.state.password !== this.state.confirmPass;
+  };
   render() {
     return (
       <Container>
-        <h2>Password Reset</h2>
-        <Row>
+        <Form>
           <Col sm='6'>
-            <input type='password' placeholder=' enter new password' onChange={e => this.setState({password: e.target.value})} />
+            <Form.Group controlId='formBasicCurrentPassword'>
+              <Form.Label column={true}>Current Password</Form.Label>
+              <Form.Control
+               type='password'
+               value={this.state.currentPass}
+               onChange={this.onCurrentPasswordChange}
+               placeholder='Current Password'
+              />
+            </Form.Group>
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Label column={true}>New Password</Form.Label>
+              <Form.Control
+               type='password'
+               value={this.state.password}
+               onChange={this.onPasswordChange}
+               placeholder='New Password'
+              />
+            </Form.Group>
+            <Form.Group controlId='formBasicConfirmPassword'>
+              <Form.Label column={true}>Confirm Password</Form.Label>
+              <Form.Control
+                isInvalid={this.isPassInvalid()}
+                type='password'
+                value={this.state.confirmPass}
+                onChange={this.onConfirmPassChange}
+                placeholder='Confirm New Password'
+              />
+              <Form.Control.Feedback type='invalid'>
+                The password did not match, please try again.
+              </Form.Control.Feedback>
+              <Button variant='primary' type='submit'>Register</Button>
+            </Form.Group>
           </Col>
-        </Row>
-        <Row>
-          <Col sm='6'>
-            <Button variant='primary' type='submit'>Reset Password</Button>
-          </Col>
-        </Row>
+        </Form>
       </Container>
     );
   }
