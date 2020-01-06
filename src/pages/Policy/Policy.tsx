a7e2f15168e7ba926563b4c4e5e21802c527f7ee
Zac Clifton <zac16530@gmail.com>
2020-01-05 19:27:59 -0500

    added a file for the polices html and added contact us to the end

--- src/pages/Policy/Policy.tsx
+++ src/pages/Policy/Policy.tsx
@@ -3,51 +3,53 @@ import * as React from 'react';
 import { Accordion, Card } from 'react-bootstrap';
 import {CssBaseline} from '@material-ui/core';
 import {Container} from '@material-ui/core';
+import styles from './Policy.module.scss';
 
 export default class ReactApp extends React.Component {
-  content0() {
-    return (
-      <div>
-      Microsoft ASP.NET is a set of technologies in the Microsoft .NET Framework for building Web applications and XML Web services.
-    </div>
-    );
-  }
   render() {
     return (
       <React.Fragment>
         <CssBaseline />
         <Container maxWidth='sm'>
-          <div>Site Policy</div>
+          <h1 className={styles['headline']}>Site Policy</h1>
           <Accordion defaultActiveKey='0'>
             <Card>
               <Accordion.Toggle as={Card.Header} eventKey='0'>
-                Click me!
+                TERMS OF USE
               </Accordion.Toggle>
               <Accordion.Collapse eventKey='0'>
-                <Card.Body>Hello! I'm the body</Card.Body>
+                <Card.Body> <div>test</div> </Card.Body>
               </Accordion.Collapse>
             </Card>
             <Card>
               <Accordion.Toggle as={Card.Header} eventKey='1'>
-                Click me!
+                PRIVACY POLICY
               </Accordion.Toggle>
               <Accordion.Collapse eventKey='1'>
                 <Card.Body>Hello! I'm another body</Card.Body>
               </Accordion.Collapse>
+             </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey='2'>
+                COOKIE POLICY
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey='2'>
+                <Card.Body>this.content0()</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey='3'>
+                DISCLAIMER
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey='3'>
+                <Card.Body>this.content0()</Card.Body>
+              </Accordion.Collapse>
             </Card>
           </Accordion>
+          <div> In order to resolve any complaint regarding the Site, or our policies,
+            please contact us at our contact us page.</div>
         </Container>
       </React.Fragment>
     );
   }
 }
-
-{/* <PanelGroup accordion={true} defaultActiveKey={1} bordered={true} className={styles['test']}>
-        <Panel header={'panel1'} eventKey={1}>
-          <div className={styles['not-found']}>this.content1</div>
-        </Panel>
-        <Panel header={'panel2'} eventKey={2}>
-          <div className={styles['not-found']}>this.content2</div>
-        </Panel>
-      </PanelGroup>
-      */}
