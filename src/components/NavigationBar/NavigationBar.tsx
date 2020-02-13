38bc986c68b22e30c643b0a6662fb5ebc745cf4f
zbouvier <zbouvier99@gmail.com>
2020-02-12 21:25:28 -0500

    Added contact us icon

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import styles from './NavigationBar.module.scss';
-import {faList, faUser} from '@fortawesome/free-solid-svg-icons';
+import {faList, faUser, faEnvelopeOpenText} from '@fortawesome/free-solid-svg-icons';
 import {connect} from 'react-redux';
 import {WebState} from '../../redux/types/WebState';
 import {isAuthenticated} from '../../redux/selectors/entities';
@@ -17,6 +17,7 @@ const NavigationBarComponent = ({authenticated}: ReturnType<typeof mapStateToPro
         {authenticated ? <NavItem label='My Modules' link='/my-modules' icon={faList} /> : null}
         {/*{authenticated ? <NavItem label='Modules' link='/modules' icon={faList} /> : null}*/}
         <NavItem label='Account' link='/login' icon={faUser} />
+        <NavItem label='Contact Us' link='/contact' icon={faEnvelopeOpenText} />
       </Nav>
     </Container>
   </Navbar>
