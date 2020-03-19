9e3baed19c34b8979d59aedefcf2176bf716ac7b
zbouvier <zbouvier99@gmail.com>
2020-03-19 03:48:48 -0400

    added icons to statuses

--- src/pages/Status/Status.tsx
+++ src/pages/Status/Status.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import {Component} from 'react';
 import {ListGroup, Col, Dropdown, Container, Row} from 'react-bootstrap';
 import {isRunning, Statuses, UserLabVm} from '../../types/UserLabVm';
-import {faPowerOff} from '@fortawesome/free-solid-svg-icons';
+import {faPowerOff, faPlay, faUndo} from '@fortawesome/free-solid-svg-icons';
 import {CenteredIcon} from '../../util/CenteredIcon';
 import * as styles from '../../components/LabEnvironment/LabEnvironment.module.scss';
 import {getIndicatorClassName} from '../../components/LabEnvironment/LabEnvironment';
 import {shutdownVm, startUpVm, stopVm, scrubVm, resetVm} from '../../api';
+import * as optionStyles from './Status.module.scss';
 
 interface StatusProps {
   vms: UserLabVm[];
@@ -57,6 +58,7 @@ export class Status extends Component<StatusProps, StatusState> {
                     className={getIndicatorClassName(isRunning(this.props.statuses[vm.id]))}
                     icon={faPowerOff}
                   />
+                  
                 </Col>
                 <Col>
                   {this.state.loadingText ?
@@ -64,11 +66,13 @@ export class Status extends Component<StatusProps, StatusState> {
                     <Dropdown drop='right'>
                       <Dropdown.Toggle id='dropdown-basic'/>
                       <Dropdown.Menu>
-                        <Dropdown.Item onClick={() => startUpVm(vm.id)}>Start Up</Dropdown.Item>
-                        <Dropdown.Item onClick={() => this.performShutdown(vm.id)}>Shutdown</Dropdown.Item>
-                        <Dropdown.Item onClick={() => stopVm(vm.id)}>Force Shutdown</Dropdown.Item>
-                        <Dropdown.Item onClick={() => this.performScrub(vm.id)}>Scrub</Dropdown.Item>
-                        <Dropdown.Item onClick={() => resetVm(vm.id)}>Reset</Dropdown.Item>
+                        <Dropdown.Item onClick={() => startUpVm(vm.id)}><CenteredIcon icon={faPlay} className='start-button'/>Start Up
+                        
+                        </Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.performShutdown(vm.id)}><CenteredIcon icon={faPowerOff} className={optionStyles['shut-down-button']}/>Shutdown</Dropdown.Item>
+                        <Dropdown.Item onClick={() => stopVm(vm.id)}><CenteredIcon icon={faPowerOff} className={optionStyles['force-shut-down-button']}/>Force Shutdown</Dropdown.Item>
+                        <Dropdown.Item onClick={() => this.performScrub(vm.id)}><CenteredIcon icon={faUndo} className={optionStyles['scrub-button']}/>Scrub</Dropdown.Item>
+                        <Dropdown.Item onClick={() => resetVm(vm.id)}><CenteredIcon icon={faUndo} className={optionStyles['reset-button']}/>Reset</Dropdown.Item>
                       </Dropdown.Menu>
                     </Dropdown>
                   }
