5a0bc550a2d81ab28d28319f832c4daa0c3a24eb
John <john.pcooke94@gmail.com>
2021-06-15 21:57:29 -0400

    Scaffold out the basic structure of the user list in the admin panel.

--- src/components/AdminPanelLayout/UsersPane.tsx
+++ src/components/AdminPanelLayout/UsersPane.tsx
@@ -1,8 +1,28 @@
-import {TabPane} from 'react-bootstrap';
+import {TabPane, ListGroup, ListGroupItem} from 'react-bootstrap';
 import React from 'react';
+
+// TODO: replace dummy data with actual data from database retrieved with API call(s)
+const users = [{'id':1, 'firstname':'John', 'lastname':'User', 'role':'Student'}, {'id':2, 'firstname':'Bill', 'lastname':'Buddy', 'role':'Admin'}];
+
+// TODO: figure out how to get this list to flow horizonally
+const userList = users.map((d) =>
+  <ListGroup key={d.id}>
+    <ListGroupItem key={d.firstname}>
+      {d.firstname}
+    </ListGroupItem>
+    <ListGroupItem key={d.lastname}>
+      {d.lastname}
+    </ListGroupItem>
+    <ListGroupItem key={d.role}>
+      {d.role}
+    </ListGroupItem>
+  </ListGroup>
+);
 
 export const UsersPane = () => (
   <TabPane eventKey='#user-management'>
-    <p>The user-management panel is still under construction</p>
+    <ListGroup>
+      {userList}
+    </ListGroup>
   </TabPane>
 );
