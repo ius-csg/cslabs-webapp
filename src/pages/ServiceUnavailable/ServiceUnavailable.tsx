3ccbdceafaa0a0667b22b506a66a4ec007be9211
Zion Emond <zionem7@gmail.com>
2021-07-02 14:26:56 -0400

    added an image to the page

--- src/pages/ServiceUnavailable/ServiceUnavailable.tsx
+++ src/pages/ServiceUnavailable/ServiceUnavailable.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import {Layout} from '../Layout/Layout';
 import styles from './ServiceUnavailable.module.scss';
+import image from '../../assets/images/broken_server.png'
 
 const ServiceUnavailable = () => (
   <Layout>
     <div className={styles['number-code']}>
+      <img src={image} alt={'broken_server.png'}/>
       <h1>503</h1>
       <h2>Service Unavailable</h2>
       <body>Sorry, our servers are unavailable at the moment. We are actively working to get
       them back up as soon as possible</body>
-      <p>Somebody ping Adam and tell him to go kick the servers</p>
+      <p>Somebody ping Dr. Doyle and tell him to go kick the servers</p>
     </div>
   </Layout>
 );
