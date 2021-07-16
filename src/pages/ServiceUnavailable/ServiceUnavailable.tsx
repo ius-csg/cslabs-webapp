75e6dd8b077cfcd3c63cff62fcab6aa2831b2fee
Zion Emond <zionem7@gmail.com>
2021-07-15 20:07:09 -0400

    started work on adaptive 503

--- src/pages/ServiceUnavailable/ServiceUnavailable.tsx
+++ src/pages/ServiceUnavailable/ServiceUnavailable.tsx
@@ -1,8 +1,24 @@
 import * as React from 'react';
 import {Layout} from '../Layout/Layout';
 import styles from './ServiceUnavailable.module.scss';
-import image from '../../assets/images/broken_server.png'
+import image from '../../assets/images/broken_server.png';
 
+function MaintenanceMessage(props) {
+  return <body>Our servers will be back up at x time</body>;
+}
+
+function DownMessage(props) {
+  return <body>Sorry, our servers are unavailable at the moment. We are actively working to get
+  them back up as soon as possible</body>;
+}
+
+function ServiceMessage(props) {
+  const isMaintenanceMode = props.isMaintenanceMode;
+  if (isMaintenanceMode) {
+    return <MaintenanceMessage />;
+  }
+  return <DownMessage />;
+}
 
 const ServiceUnavailable = () => (
   <Layout>
@@ -10,8 +26,7 @@ const ServiceUnavailable = () => (
       <img src={image} alt={'broken_server.png'}/>
       <h1>503</h1>
       <h2>Service Unavailable</h2>
-      <body>Sorry, our servers are unavailable at the moment. We are actively working to get
-      them back up as soon as possible</body>
+      <ServiceMessage />
       <p>Somebody ping Dr. Doyle and tell him to go kick the servers</p>
     </div>
   </Layout>
