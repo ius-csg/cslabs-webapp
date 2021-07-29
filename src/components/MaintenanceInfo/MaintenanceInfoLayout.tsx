a44a9d695085cd6c0fd91a19447e68632a8db569
Zion Emond <zionem7@gmail.com>
2021-07-29 13:36:55 -0400

    refactored code so most of it was it its own component that is then rendered in a different file. Added API call for maintenance information

---
+++ src/components/MaintenanceInfo/MaintenanceInfoLayout.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Layout} from '../../pages/Layout/Layout';
+import styles from './MaintenanceInfoLayout.module.scss';
+import image from '../../assets/images/broken_server.png';
+import {useMount} from '../../hooks/useMount';
+import {useState} from 'react';
+import {getMaintenances} from '../../api';
+import {Maintenance} from '../../types/Maintenance';
+
+const ServiceMessage = (props: Maintenance) => {
+  if (props.isMaintenanceMode) {
+    if (props.isRestorationTimeKnown) {
+      return <div>Our servers will be back up at {props.restorationTime}</div>;
+    }
+  }
+  return <div>Sorry, our servers are unavailable at the moment. We are actively working to get
+    them back up as soon as possible.</div>;
+};
+
+const MaintenanceInfoLayout = () => {
+
+  const [maintenance, setMaintenance] = useState();
+
+  useMount( async () => {
+    setMaintenance(getMaintenances());
+  });
+
+  return (
+    <Layout>
+      <div className={styles['number-code']}>
+        <img src={image} alt={'broken_server.png'}/>
+        <h1>503</h1>
+        <h2>Service Unavailable</h2>
+        <ServiceMessage isMaintenanceMode={maintenance.isMaintenanceMode} isRestorationTimeKnown={false}/>
+        <div>For more information, please contact your local CSG administrator</div>
+      </div>
+    </Layout>
+  );
+};
+export default MaintenanceInfoLayout;
+
+
--- src/pages/ServiceUnavailable/ServiceUnavailable.tsx
+++ src/pages/ServiceUnavailable/ServiceUnavailable.tsx
@@ -1,35 +1,11 @@
 import * as React from 'react';
 import {Layout} from '../Layout/Layout';
-import styles from './ServiceUnavailable.module.scss';
-import image from '../../assets/images/broken_server.png';
-import {useState} from 'react';
-
-interface Props {
-  isMaintenanceMode: boolean;
-  isRestorationTimeKnown: boolean;
-  restorationTime?: number;
-}
-
-const ServiceMessage = (props: Props) => {
-  if (props.isMaintenanceMode) {
-    if (props.isRestorationTimeKnown) {
-      return <body>Our servers will be back up at {props.restorationTime}</body>;
-    }
-  }
-  return <body>Sorry, our servers are unavailable at the moment. We are actively working to get
-  them back up as soon as possible.</body>;
-};
-
-const [maintenance, setMaintenance] = useState<Props>();
+import MaintenanceInfoLayout from '../../components/MaintenanceInfo/MaintenanceInfoLayout';
 
 const ServiceUnavailable = () => (
   <Layout>
-    <div className={styles['number-code']}>
-      <img src={image} alt={'broken_server.png'}/>
-      <h1>503</h1>
-      <h2>Service Unavailable</h2>
-      <ServiceMessage isMaintenanceMode={true} isRestorationTimeKnown={false} />
-      <p>Somebody ping Dr. Doyle and tell him to go kick the servers</p>
+    <div>
+      <MaintenanceInfoLayout />
     </div>
   </Layout>
 );
