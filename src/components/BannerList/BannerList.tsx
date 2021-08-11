4d67aeb7c936a7215c9229dc5b6e64f630a054e4
astin <astiner21@gmail.com>
2021-08-11 10:23:03 -0400

    Only creates one banner and then does not create the others in array.

--- src/components/BannerList/BannerList.tsx
+++ src/components/BannerList/BannerList.tsx
@@ -1,27 +1,36 @@
 import React, {useState} from 'react';
 import {AlertNotification, AlertNotificationTypes} from '../Alert/AlertNotification';
 
+
 interface BannerListProp {
   type: AlertNotificationTypes;
   description: string;
 
 }
 
-function BannerList (prop: BannerListProp) {
-  const [count, setCount] = useState(0);
+const BannerList  = () => {
 
   const BL: BannerListProp[] = [
-    {type: 'warning', description: 'Test1'},
-    {type: 'info', description: 'Test2'},
-    {type: 'notice', description: 'Test3'}
+    { type: 'warning', description: 'Test1'},
+    { type: 'info', description: 'Test2'},
+    { type: 'notice', description: 'Test3'}
   ];
+
+
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      {BL[0] ? <AlertNotification onClick={() => setCount(count + 1)}  description={prop.description} type={prop.type}/>
+      {BL[count] ? <AlertNotification onClick={() => setCount(count + 1)} type={BL[count].type} description={BL[count].description} />
         : null}
     </div>
   );
 };
 
 export default BannerList;
+
+
+
+
+// export const BannerList: React.FC<BannerListProp> = ({type, description}) =>
+//  return
