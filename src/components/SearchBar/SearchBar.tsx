4f8e5776bbd4e28ca4ece05d0245abb57a3b3cba
zkhussain <kzhzaid02@gmail.com>
2021-09-21 22:09:05 -0400

    Add api call

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -4,10 +4,22 @@ import styles from './SearchBar.module.scss';
 import {Form} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import ToolTip from '../util/ToolTip';
+import { searchModules } from 'api';
 
 const SearchBar = (props : any) => {
 
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const getModuleRequest = async (searchValue : any) => {
+        try {
+          if (searchTerm.length !== 0) {
+            let response = await searchModules(searchValue);
+            props.showModules(response)
+          }
+        } catch (error) {
+          console.error(error);
+        }
+    };
 
 
 
