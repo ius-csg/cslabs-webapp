2b511a6012651b31ae9132c8c3f129d746c5a813
Justin Hurst <hurstjustin502@gmail.com>
2021-10-03 21:54:47 -0400

    Created component for switch button to remove duplicate logic

--- src/components/LabEditorGUI/SelectSwitch.tsx
+++ src/components/LabEditorGUI/SelectSwitch.tsx
@@ -11,6 +11,7 @@ import TwentyFourPort from '../../assets/images/LabEditor/Twenty-FourPortSwitch.
 import FortyEightPort from '../../assets/images/LabEditor/Forty-EightPortSwitch.png';
 import closeIcon from '../../assets/icons/close.svg';
 import styles from './SelectSwitch.module.scss';
+import SwitchButton from './SwitchButton';
 
 const SelectSwitch = ({close, addSwitch}: any) => {
   const handleSelect = (ports: number) => {
@@ -25,36 +26,11 @@ const SelectSwitch = ({close, addSwitch}: any) => {
         </button>
         <h2 style={{display: 'flex', justifyContent: 'center', paddingTop:'1em'}}>Select a Switch</h2>
         <div style={{display: 'flex', justifyContent: 'center'}}>
-          <div className={styles.switchContainer}>
-            <button onClick={() => handleSelect(5)} style={{height: '8em', border:'none', background:'none'}}>
-              <img src={FivePort} alt='FivePort' style={{height: '4.75em'}}/>
-            </button>
-            5 Port
-          </div>
-          <div className={styles.switchContainer}>
-            <button onClick={() => handleSelect(8)} className={styles.switchButton}>
-              <img src={EightPort} alt='EightPort' style={{height: '4.75em'}}/>
-            </button>
-            8 Port
-          </div>
-          <div className={styles.switchContainer}>
-            <button onClick={() => handleSelect(16)} className={styles.switchButton}>
-              <img src={SixteenPort} alt='SixteenPort' style={{height: '7em'}}/>
-            </button>
-            16 Port
-          </div>
-          <div className={styles.switchContainer}>
-            <button onClick={() => handleSelect(24)} className={styles.switchButton}>
-              <img src={TwentyFourPort} alt='TwentyFourPort' style={{height: '7em'}}/>
-            </button>
-            24 Port
-          </div>
-          <div className={styles.switchContainer}>
-            <button onClick={() => handleSelect(48)} className={styles.switchButton}>
-              <img src={FortyEightPort} alt='FortyEightPort' style={{height: '7em'}}/>
-            </button>
-            48 Port
-          </div>
+          <SwitchButton handleSelect={() => handleSelect(5)} imageSrc={FivePort} portSize={5}/>
+          <SwitchButton handleSelect={() => handleSelect(8)} imageSrc={EightPort} portSize={8}/>
+          <SwitchButton handleSelect={() => handleSelect(16)} imageSrc={SixteenPort} portSize={16}/>
+          <SwitchButton handleSelect={() => handleSelect(24)} imageSrc={TwentyFourPort} portSize={24}/>
+          <SwitchButton handleSelect={() => handleSelect(48)} imageSrc={FortyEightPort} portSize={48}/>
         </div>
       </div>
   );
