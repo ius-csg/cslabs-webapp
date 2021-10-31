a0e2e73c1ea276bc4cfd2b508897cfc9edc86b5a
zkhussain <kzhzaid02@gmail.com>
2021-10-31 11:12:10 -0400

    Fix code smells

--- src/components/LabEnvironment/LabEnvironment.tsx
+++ src/components/LabEnvironment/LabEnvironment.tsx
@@ -144,7 +144,7 @@ export class LabEnvironment extends Component<LabEnvironmentProps, LabEnvironmen
                 loading={this.props.starting}
                 label='Start Lab'
                 onClick={this.props.onStartLab} 
-              /> : <h6 style={{textAlign: 'right'}}>Lab's time remaining: {getRemainingLabTime(this.state.labEndDateTime!)}
+              /> : <h6 style={{textAlign: 'right'}}>Lab's time remaining: {getRemainingLabTime(this.state.labEndDateTime)}
               {this.showExtendButton()}</h6>}
           </Row>
           <Row className='fill-height'>
