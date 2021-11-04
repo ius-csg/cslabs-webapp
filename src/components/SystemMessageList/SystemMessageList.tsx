c04b8c74b1e98af9312e8740d2eca2ee16dece6c
astin <astiner21@gmail.com>
2021-11-03 23:54:08 -0400

    Added comments

--- src/components/SystemMessageList/SystemMessageList.tsx
+++ src/components/SystemMessageList/SystemMessageList.tsx
@@ -17,16 +17,23 @@ const SystemMessagesList = () => {
   const [dismissedMessage, setDismissedMessage] = useState<string[]>([]);
   const [currentMessage, setCurrentMessage] = useState<SystemMessageList>();
 
-  useEffect(() => {
+  useEffect(() => {// when component mountes this is called.
+    // Finds current messages that are not contained in the local storage
     setCurrentMessage(systemMessageList.find(message => !dismissedMessage.includes(message.id.toString())));
+
+    // Check dismissed message state if contains items before overwrinting the local item storage with the it.
     if (dismissedMessage.length > 0) {
       window.localStorage.setItem('dismissedMessage', dismissedMessage.join());
     }
   }, [dismissedMessage]);
 
-  useMount(async () => {
+  useMount(async () => {// called when page is rendered
+    // Gets the system messages list from the backend
     const messages = await getSystemMessages();
     setSystemMessageList(messages);
+
+    // Checks if there are dismissed IDs in local storage, and if so set the
+    // component states appropriately so that those messages don't render.
     const localStorageItem = window.localStorage.getItem('dismissedMessage');
     if (localStorageItem) {
       const localStorageArray = localStorageItem.split(',');
@@ -34,6 +41,7 @@ const SystemMessagesList = () => {
       setCurrentMessage(messages.find(message => !localStorageArray.includes(message.id.toString())));
     }
     else {
+      // If nothing in local storage render the first message
       setCurrentMessage(messages[0]);
     }
   });
