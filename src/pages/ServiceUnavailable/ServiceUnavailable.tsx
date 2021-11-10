b9cde2249f67741efb8992cb1afb0f14066762ba
Zion Emond <zionem7@gmail.com>
2021-11-10 16:20:53 -0500

    changed so either the maintenance message or the down message will be shown. If there is no maintenance information retreived from the database, then the default down message will be shown

--- src/pages/ServiceUnavailable/ServiceUnavailable.tsx
+++ src/pages/ServiceUnavailable/ServiceUnavailable.tsx
@@ -28,12 +28,12 @@ const ServiceUnavailable = () => {
         <img src={image} alt={'broken_server.png'}/>
         <h1>503</h1>
         <h2>Service Unavailable</h2>
-        {maintenances && maintenances.length > 0 &&
-          <ServiceMessage
+        {maintenances && maintenances.length > 0 && !isErrored
+          ? <ServiceMessage
             startTime={maintenances[0].startTime}
             endTime={maintenances[0].endTime}
-          /> }
-        {isErrored || maintenances !== undefined &&
+          />
+          :
           <div>Sorry, our servers are unavailable at the moment. We are actively working to get them back up as soon as possible.</div>}
         <div>For more information, please contact your local CSG administrator</div>
       </div>
