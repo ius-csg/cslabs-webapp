dd43c65317ff13bf06df2a6ebe7810f788c5eb8e
Zion Emond <zionem7@gmail.com>
2021-11-12 14:39:02 -0500

    modified ServiceMessage component to display date and time in a cleaner format

--- src/components/ServiceMessage/ServiceMessage.tsx
+++ src/components/ServiceMessage/ServiceMessage.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Maintenance} from '../../types/Maintenance';
+
 const ServiceMessage = (props: Maintenance) => {
-   return <div>Our servers are currently down for maintenance and are scheduled to be back up
-        at {props.endTime}</div>;
+
+  const end = new Date(props.endTime);
+  const currentDate = new Date();
+
+  if (end.toDateString() === currentDate.toDateString()) {
+    return <div>Our servers are currently down for maintenance and are scheduled to be back up
+      at {end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}.</div>;
+  }
+  else {
+    return <div>Our servers are currently down for maintenance and are scheduled to be back up
+      on {end.toDateString()} at {end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}.</div>;
+  }
+
 };
 export default ServiceMessage;
