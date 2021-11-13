e3373b4ffed971883bcea12e4286c948444ca07f
astin <astiner21@gmail.com>
2021-11-13 13:00:00 -0500

    Added react icon to dev dependencies, added description and formatting to Email Verification page.

--- src/pages/Email_Verification/Email_Verification.tsx
+++ src/pages/Email_Verification/Email_Verification.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
-import styles from './Email_Verification.scss';
+import styles from './Email_Verification.module.scss';
 import {Layout} from '../Layout/Layout';
+import {RiCheckboxCircleFill} from 'react-icons/ri';
 
 
-const emailVerification = () => (// add verification instead of valid, remove page and reminder
+function emailSent() {
+  alert('Email Sent');
+}
+
+const emailVerification = () => (
   <Layout>
-    <div className={styles['email-verification']}>
-      <label>200</label>
+      <div className={styles['email-verification']}>
+      <p className={styles['title']}>
+        <RiCheckboxCircleFill className={styles['check']}/>
+        A verification link has been sent to your email
+      </p>
+      <p className={styles['paragraph']}>
+        Please go to your email account and click on the link that has just been sent to you to verify your account.</p>
+      <button onClick={emailSent}>Send another verification email</button>
     </div>
   </Layout>
 );
