79d346790eba7f86d8376716ea0405409d708880
Jason Henry <jasdhenr@iu.edu>
2021-11-17 13:27:47 -0500

    fixing branch issues

--- src/components/TagEditor/TagEditor.tsx
+++ src/components/TagEditor/TagEditor.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import ReactTags, {Tag} from 'react-tag-autocomplete';
+import ReactTags, {Tag, ClassNames} from 'react-tag-autocomplete';
 import {} from './TagEditor.scss';
 
 interface Props {
@@ -16,14 +16,14 @@ export function TagEditor({tags, tagSuggestions, onInput, onAdd, onDelete, mes, 
   const [className, setClassName] = useState('disabled');
 
   useEffect(() => {
-      if (mes === 'success' && editing)
-        setClassName('enabled success');
-      else if (mes === 'success' && !editing)
-        setClassName('disabled success');
-      else if (!editing)
-        setClassName('disabled');
-      else
-        setClassName('enabled');
+    if (mes === 'success' && editing)
+      setClassName('enabled success');
+    else if (mes === 'success' && !editing)
+      setClassName('disabled success');
+    else if (!editing)
+      setClassName('disabled');
+    else
+      setClassName('enabled');
   }, [mes, editing]);
 
   function onValidate(tag: Tag) {
@@ -31,6 +31,19 @@ export function TagEditor({tags, tagSuggestions, onInput, onAdd, onDelete, mes, 
     tags.forEach(t => {if (t.name === tag.name) valid = false;});
     return valid;
   }
+
+  const classNames: ClassNames = {
+    root: 'react-tags',
+    rootFocused: 'is-focused',
+    selected: 'react-tags-selected',
+    selectedTag: 'react-tags-selected-tag',
+    selectedTagName: 'react-tags-selected-tag-name',
+    search: 'react-tags-search',
+    searchInput: 'react-tags-search-input',
+    suggestions: 'react-tags-suggestions',
+    suggestionActive: 'is-active',
+    suggestionDisabled: 'is-disabled'
+  };
 
   return (
     <div className={className}>
@@ -42,6 +55,7 @@ export function TagEditor({tags, tagSuggestions, onInput, onAdd, onDelete, mes, 
         suggestions={tagSuggestions}
         allowNew={true}
         onValidate={onValidate}
+        classNames={classNames}
       />
     </div>
   );
