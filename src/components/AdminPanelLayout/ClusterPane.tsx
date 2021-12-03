c54d998143aa9c6b23644718b8482256c86aefe9
Zion Emond <zionem7@gmail.com>
2021-12-03 15:07:43 -0500

    added buttons and some text

--- src/components/AdminPanelLayout/ClusterPane.tsx
+++ src/components/AdminPanelLayout/ClusterPane.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import {Layout} from '../../pages/Layout/Layout';
+import styles from './ClusterPane.module.scss';
 
 export const ClusterPane = () => (
-  <>The cluster management panel is still under construction</>
+  <Layout>
+    <div className={styles['cluster-pane']}>
+      <Button>Set Maintenance Mode</Button>
+      <Button>Remove Node</Button>
+      <Button>Add Node</Button>
+      <p>------------------- &lt;Cluster Name&gt; -------------------</p>
+    </div>
+  </Layout>
 );
