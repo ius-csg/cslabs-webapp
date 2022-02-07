8a02e71665363ce420527f05b6daa9d4b769b67e
Zion Emond <zionem7@gmail.com>
2022-02-07 11:51:01 -0500

    added Maintenance Mode popup window

--- src/components/AdminPanelLayout/ClusterPane.tsx
+++ src/components/AdminPanelLayout/ClusterPane.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from 'react-bootstrap';
 import {Layout} from '../../pages/Layout/Layout';
 import styles from './ClusterPane.module.scss';
 import Node from './Node';
+import MaintenancePopup from './MaintenancePopup';
 
-export const ClusterPane = () => (
-  <Layout>
-    <div className={styles['cluster-pane']}>
-      <Button>Set Maintenance Mode</Button>
-      <Button>Remove Node</Button>
-      <Button>Add Node</Button>
-      <p>------------------- CLUSTER NAME -------------------</p>
-    </div>
-    <Node nodeNum={1} statusNum={0}/>
-    <Node nodeNum={2} statusNum={1}/>
-    <Node nodeNum={3} statusNum={2}/>
-    <Node nodeNum={4} statusNum={3}/>
-  </Layout>
-);
+
+export const ClusterPane = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (<Layout>
+      <div className={styles['cluster-pane']}>
+        <Button onClick={togglePopup}>Set Maintenance Mode</Button>
+        {isOpen && <MaintenancePopup
+          content={
+            <>
+              <h2>Maintenance Scheduler</h2>
+              <form>
+                <label>Node #
+                  <textarea>N/A</textarea>
+                </label>
+                <label>Node #
+                  <textarea>N/A</textarea>
+                </label>
+                <label>Node #
+                  <textarea>N/A</textarea>
+                </label>
+                <Button>Confirm</Button>
+              </form>
+            </>
+          }
+          handleClose={() => togglePopup()}
+        />
+        }
+        <Button>Remove Node</Button>
+        <Button>Add Node</Button>
+        <p>------------------- CLUSTER NAME -------------------</p>
+      </div>
+      <Node nodeNum={1} statusNum={0}/>
+      <Node nodeNum={2} statusNum={1}/>
+      <Node nodeNum={3} statusNum={2}/>
+      <Node nodeNum={4} statusNum={3}/>
+    </Layout>);
+};
