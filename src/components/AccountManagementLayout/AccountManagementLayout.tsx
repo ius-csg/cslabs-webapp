2134deefb2f9acc359fe3c997303d34ebc0dd3d3
Jason Gallavin <jasongallavin@gmail.com>
2022-04-10 14:24:47 -0400

    Fix routing issue on profile page

--- src/components/AccountManagementLayout/AccountManagementLayout.tsx
+++ src/components/AccountManagementLayout/AccountManagementLayout.tsx
@@ -4,6 +4,8 @@ import {Layout} from '../../pages/Layout/Layout';
 import {RoutePaths} from 'router/RoutePaths';
 import {faUser, faKey, faNewspaper} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {matchPath} from 'react-router';
+import { useHistory } from 'react-router-dom';
 
 interface AccountManagementLayoutProps {
   children: any;
@@ -14,32 +16,44 @@ const accountManagementOptions = [
   {label: 'Change Password', pageLink: RoutePaths.resetPassword, icon: faKey},
   {label: 'Newsletter Status', pageLink: RoutePaths.newsletterStatus, icon: faNewspaper}
 ];
+const getActiveKey = (url: string) => getFirstItem(accountManagementOptions.map(o => o.pageLink), o => matchPath(url, o) !== null);
 
-export const AccountManagementLayout = (props: AccountManagementLayoutProps) => (
-  <Layout>
-    <h1>Account Management</h1>
-    <Tab.Container id='user-account-management' activeKey={window.location.pathname}>
-      <Row style={{marginTop: '1.5rem'}}>
-        <Col sm={4}>
-          <ListGroup>
-            {accountManagementOptions.map(option => (
-              <ListGroup.Item action={true} href={option.pageLink} key={option.pageLink}>
-                <FontAwesomeIcon icon={option.icon} className='mr-3' />
-                {option.label}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Col>
-        <Col sm={8}>
-          <Tab.Content>
-            {accountManagementOptions.map(option => (
-              <Tab.Pane eventKey={option.pageLink} key={option.pageLink}>  
-                {props.children}
-              </Tab.Pane>
-            ))}
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
-  </Layout>
-);
+function getFirstItem<T>(items: T[], filter: (i: T) => boolean) {
+  const filtered = items.filter(filter);
+  if(filtered.length > 0)
+    return filtered[0];
+  return undefined;
+}
+
+export function AccountManagementLayout(props: AccountManagementLayoutProps) {
+  const history = useHistory();
+  return (
+    <Layout>
+      <h1>Account Management</h1>
+      <Tab.Container id='user-account-management' activeKey={getActiveKey(window.location.pathname)}>
+        <Row style={{marginTop: '1.5rem'}}>
+          <Col sm={4}>
+            <ListGroup>
+              {accountManagementOptions.map(option => (
+                <ListGroup.Item action={true}  onClick={() => history.push(option.pageLink)} key={option.pageLink}>
+                  <FontAwesomeIcon icon={option.icon} className='mr-3' />
+                  {option.label}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col sm={8}>
+            <Tab.Content>
+              {accountManagementOptions.map(option => (
+                <Tab.Pane eventKey={option.pageLink} key={option.pageLink}>
+                  {props.children}
+                </Tab.Pane>
+              ))}
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </Layout>
+  );
+}
+
